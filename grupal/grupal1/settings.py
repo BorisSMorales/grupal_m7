d3bbfe903e09a424f936a48f302ff1452dab63e2
BorisSMorales <salvadormoralesacuna@gmail.com>
2023-07-01 23:02:51 -0400

    Update settings.py

--- grupal/grupal1/settings.py
+++ grupal/grupal1/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_test',
-        'USER': 'usuario_test',
+        'USER': 'usuariotest',
         'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '5432',

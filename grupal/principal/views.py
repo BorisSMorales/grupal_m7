4ef1d3eeea115c2363f7ca47d2ab533f0a74ef75
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-07-05 05:20:24 -0300

    detalle de pedido actualizado al agregar productos

--- grupal/principal/views.py
+++ grupal/principal/views.py
@@ -324,14 +324,15 @@ class DetallesPedidoView(View):
     def get(self, request, pedido_id):
         pedido = get_object_or_404(Pedido, id=pedido_id)
         detalles = DetallePedido.objects.filter(pedido=pedido)
-        direccion_envio = request.session.get('direccion_envio')
+        direccion_envio = pedido.direccion_cliente.direccion  # Acceder a la dirección de envío
+        
         context = {
             'pedido': pedido,
             'detalles': detalles,
             'direccion_envio': direccion_envio,
         }
         return render(request, 'telovendo3app/detalle_pedido.html', context)
-    
+
 
 class SeleccionarDireccionView(View):
     template_name = 'telovendo3app/direcciones_cliente.html'

ff8e1f0338fdbdb914b22595348e9a046003a242
José Ramos <joseramoslewis@gmail.com>
2023-07-09 04:06:50 -0400

    update

--- grupal/grupal1/urls.py
+++ grupal/grupal1/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from principal.views import CrearPedidoView

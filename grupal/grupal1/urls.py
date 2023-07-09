b71cf5217d28209dad59c26e59b80b34b7cbf535
José Ramos <joseramoslewis@gmail.com>
2023-07-09 04:07:00 -0400

    update

--- grupal/grupal1/urls.py
+++ grupal/grupal1/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path
@@ -45,3 +46,5 @@ urlpatterns = [
     path('detalle-pedido_gestion/<int:pedido_id>/', DetallePedidoGestionView.as_view(), name='detalle_pedido'),
     path('agregar-detalle-pedido/<int:pedido_id>/', AgregarDetallePedidoView.as_view(), name='agregar_detalle_pedido'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
